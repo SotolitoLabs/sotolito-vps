a5bd172b95a3cce914c463dc49debee25f3c6388
Ivan Chavero <ichavero@chavero.com.mx>
2020-03-10 15:58:44 -0700

    MISC: Add create letsencrypt cert script

---
+++ create_cert.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+HAPROXY_SSL="/etc/ssl/haproxy/"
+LETSENCRYPT_BASE="/etc/letsencrypt/live"
+
+if [[ "${1}" == "" ]]; then
+	echo "Missing domain argument"
+	exit
+fi
+
+if [[ "${2}" == "" ]]; then
+	echo "Missing webroot argument"
+	exit
+fi
+
+DOMAIN=$1
+WEBROOT=$2
+
+echo "Creating certificate for: ${DOMAIN}"
+echo "/usr/bin/certbot-3 certonly --webroot -w $WEBROOT -d $DOMAIN --webroot $WEBROOT"
+/usr/bin/certbot-3 certonly --webroot -w $WEBROOT -d $DOMAIN
+cd $LETSENCRYPT_BASE/$DOMAIN
+cat privkey.pem > haproxy_fullchain.pem
+cat fullchain.pem >> haproxy_fullchain.pem
+cd $HAPROXY_SSL
+ln -s $LETSENCRYPT_BASE/$DOMAIN/haproxy_fullchain.pem $DOMAIN
